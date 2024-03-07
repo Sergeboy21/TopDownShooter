bb4b32dc2bcb7e7f157666649105ac1852962e9f
Ghostyy <sergeimk05@gmail.com>
2024-03-07 12:22:07 +0100

    elo

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -13,6 +13,30 @@ public class EnemyHealth : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    private void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            TakeDamagee(0);
+        }
+    }
+
+    public void TakeDamagee(int source)
+    {
+        if (source == 0) // bullet
+        {
+            currentHealth -= 3;
+        }
+    }
+
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
