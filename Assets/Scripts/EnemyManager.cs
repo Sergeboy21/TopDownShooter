dde7fe833b11b75666613a85e86d7be034ff8105
Ghostyy <sergeimk05@gmail.com>
2024-03-14 14:25:25 +0100

    elo

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int totalEnemies;
+
+    void Start()
+    {
+        totalEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+
+    public void EnemyDefeated()
+    {
+        totalEnemies--;
+        if (totalEnemies <= 0)
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene("Won");
+    }
+}
